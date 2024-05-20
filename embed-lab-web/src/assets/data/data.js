88ba773bbad2691abe9771f2b0fffe4ab928a4fd
bblueberries <tata10433@gmail.com>
2024-05-20 13:18:56 +0700

    add threshold variable

---
+++ embed-lab-web/src/assets/data/data.js
@@ -0,0 +1,1 @@
+export const speedThreshold = 70;
--- embed-lab-web/src/components/SpeedCard.jsx
+++ embed-lab-web/src/components/SpeedCard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { speedThreshold } from "../assets/data/data";
 export default function SpeedCard({ value }) {
   let backgroundColor = "bg-green-200";
   let speed = value;
   let text = "";
-  if (value > 70) {
+  if (value > speedThreshold) {
     backgroundColor = "bg-red-400";
     text = "exceed limit";
   }
