8b6bbb01b94d4c9ad049fb918a0ed41caf2b72d1
tanasinp <wavet888@gmail.com>
2024-05-23 21:52:53 +0700

    change image from public to assets

--- embed-lab-web/src/App.jsx
+++ embed-lab-web/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import SpeedCard from "./components/SpeedCard";
 import database from "./utils/firebase";
+import mortorcycle from "./assets/motorcycle.png"
 import { onValue, ref } from "firebase/database";
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
         <Header />
         <SpeedCard value={speed} />
         <div className="flex flex-col my-10 items-center">
-          <img src="public/motorcycle.png" width="600" height="600" />
+          <img src={mortorcycle} width="600" height="600" />
         </div>
       </div>
     </>
